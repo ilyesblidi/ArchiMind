589a5f0b3b2a6855e29b6794b878906d939ff0e2
ilyesblidi <ll_blidi@esi.dz>
2023-02-27 00:49:25 +0100

    first commit

---
+++ yyyy/yyyy/Model/PageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yyyy.Model
+{
+    public class PageModel
+    {
+
+
+
+
+    }
+}
